fe653930b8b38670fac6295a02bdb693524d362c
joaogabiazon <jbiazonferreira@gmail.com>
2025-01-31 11:35:41 -0300

    monorepo with go, nx

---
+++ apps/users/db.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+)
+
+func Connect() *mongo.Client {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return client
+}
